341b7bab45bebea895136dc37a169ad9940f7503
Aaron Martin <aaron@eri.ucsb.edu>
2016-04-27 16:11:21 -0700

    object oriented version:   * created pluralize function so did not have to duplicate the code in print inventory.     ** utilize the pluralize function in two places now.     ** probably a way to make it one line   * modified the sell apple setup to prompt user for how many apples to sell   * the prompt for sell apples, utilizes the re module (regular expressions ie: regex) to verify its an int   * the logic actually becomes simpler, avoiding duplication of the apples sales calculation   * added some comments for Aja' edification   * remove some extraneous code from during dev

--- BasicPyGame-am-object.py
+++ BasicPyGame-am-object.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+import re
 yes = ['Y','y','yes','Yes']
 no = ['N','n','no','No']
 quit = ['X','x','Q','q']
@@ -9,7 +10,8 @@ class Game:
         self.apples = 0
         self.gold = 0
         self.name = ''
-        self.delay = 1
+        self.delay = 0
+        #self.running = True   # we could create a variable that is evaluated in the main loop
         
         self.start()
         
@@ -23,13 +25,25 @@ class Game:
         if choice in yes:
             print "Let's get started!"
             self.initiateLoop()
-        elif choice in no or choice in quit:
-            print "Okay, bye..."
-            exit()
         else:
-            exit()
+            self.bye()
             
+        ### no real need to specifically check for anything other than yes
+        ### also no need to specificall do exit here, it will return and exit on its own
+        ##elif choice in no or choice in quit:
+        ##    print "Okay, bye..."
+        ##    exit()
+        ##else:
+        ##    exit()
+        
+    def bye(self,message = "Okay, bye"):  # basically prints message, if not provided, there is a default.
+        print message
+        exit(0)
+    
+    # we COULD just loop on self.running and basically change that when needed...
+    # pros and cons
     def initiateLoop(self):
+        # while self.running:    # use self.running to control loop.
         while True:
             self.begin()
 
@@ -41,22 +55,42 @@ class Game:
         print("Pick an apple")
         self.apples += 1
         self.printInventory()
+        
+    def askHowManyApplesToSell(self):
+        howmany = raw_input("How many apples do you want to sell? ")
+        # have to verify that string contains only ints (use regex via import re)
+        mo = re.match(r'^[0-9]+$',howmany)
+        if not mo:
+            print "I was unable to recognize your entry as a number, sorry, try again later."
+        return int(mo.group(0)) if mo else 0        
     
     def sellApples(self):
         self.printInventory()
+        # reworked this to prompt user for how many apples to sell!!
+        # notice that asside from doing the regex, that the logic is simpler
+        toSell = self.askHowManyApplesToSell()
+        if toSell > self.apples:
+            print "You don't have that many apples, but we can sell all you have!"
+            toSell = self.apples
+                
         if self.apples < 1:
             print("Sorry, " +self.name+ " . You have no apples to sell.")
-        elif self.apples == 1:
-            print("You've sold your apple.")
-            self.gold += self.apples*10
-        else:
-            print("You've sold your apples.")
-            self.gold += self.apples*10
-        self.apples = 0
+            
+        suff= '' if toSell == 1 else 's'
+        self.gold += toSell*10
+        self.apples -= toSell
+        # use our own pluralize function to pluralize, so we dont have to check 
+        # each time for how many apples.
+        print "You've sold",toSell,self.pluralize('apple',toSell)+"."
+        
+        #else:
+        #    print("You've sold your apples.")
+        #    self.gold += self.apples*10
+        #self.apples = 0
+        self.printInventory()
     
     def quit(self):
-        print("Okay, we are quitting by request.")
-        exit(0)
+        self.bye("Okay, we are quitting by request.")  # call bye, but change message
 
     def checkOnSale(self):
         sell = self.ynPrompt("Do you want to sell your apples?")
@@ -77,9 +111,13 @@ class Game:
             self.checkOnSale()
         elif pick in quit:
             self.quit()
-
+            
+    def pluralize(self,item,num):
+        suff = '' if num == 1 else 's'
+        return item+suff
+        
     def printInventory(self):        
         suff = '' if self.apples == 1 else 's'
-        print "You currently have,",self.apples,'apple'+suff," and",self.gold,"gold"
+        print "You currently have,",self.apples,self.pluralize('apple',self.apples),"and",self.gold,"gold"
 
 g = Game()
